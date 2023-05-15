7660c2ba0fa43debc8d8911a25e82d635587f842
gyeoul <clsmay.1st@gmail.com>
2023-05-15 09:37:24 +0900

    Initial Commit

---
+++ src/main/java/loverduck/clover/CloverApplication.java
@@ -0,0 +1,13 @@
+package loverduck.clover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloverApplication.class, args);
+    }
+
+}
