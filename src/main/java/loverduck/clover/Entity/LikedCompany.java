12ad70494cef223f46898c4ca7b993cee0994ebc
gyeoul <clsmay.1st@gmail.com>
2023-05-15 15:11:06 +0900

    Add Liked Entity

---
+++ src/main/java/loverduck/clover/Entity/LikedCompany.java
@@ -0,0 +1,20 @@
+package loverduck.clover.Entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LikedCompany {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Company company;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Users user;
+}
---
+++ src/main/java/loverduck/clover/Entity/LikedFunding.java
@@ -0,0 +1,23 @@
+package loverduck.clover.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LikedFunding {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Funding funding;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Users user;
+}
