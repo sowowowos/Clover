db335e65c9784a00cc86fce90400ffa0df7c589d
AyoungJo <joahae0302@gmail.com>
2023-05-20 14:36:48 +0900

    feat : 아임포트 API 결제 DB 연동 1차 #40

--- src/main/java/loverduck/clover/config/QueryDslConfig.java
+++ src/main/java/loverduck/clover/config/QueryDslConfig.java
@@ -1,12 +1,14 @@
 package loverduck.clover.config;
 
-import com.querydsl.core.annotations.Config;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @Configuration
 public class QueryDslConfig {
@@ -17,4 +19,19 @@ public class QueryDslConfig {
     public JPAQueryFactory qFactory() {
         return new JPAQueryFactory(entityManager);
     }
+    
+   //Thymeleaf 다중 경로 설정 
+	/*
+	 * @Bean public ClassLoaderTemplateResolver secondaryTemplateResolver() {
+	 * ClassLoaderTemplateResolver secondaryTemplateResolver = new
+	 * ClassLoaderTemplateResolver();
+	 * secondaryTemplateResolver.setPrefix("templates/mypage/");
+	 * secondaryTemplateResolver.setSuffix(".html");
+	 * secondaryTemplateResolver.setTemplateMode(TemplateMode.HTML);
+	 * secondaryTemplateResolver.setCharacterEncoding("UTF-8");
+	 * secondaryTemplateResolver.setOrder(1);
+	 * secondaryTemplateResolver.setCheckExistence(true);
+	 * 
+	 * return secondaryTemplateResolver; }
+	 */
 }
