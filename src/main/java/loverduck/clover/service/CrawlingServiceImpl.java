9c698c508141e531e1f500ac0ef2f979f7b985d3
Gyeoul <clsmay.1st@gmail.com>
2023-05-21 02:10:31 +0900

    feat : 크롤링 서비스 기능 구체화
    - getFinancialData를 더 정확한 데이터를 반환하게 구현
    - 타입 확장성을 위해 ? 타입으로 변경, 추후 super/extend로 범위 제한해야 함

--- src/main/java/loverduck/clover/service/CrawlingServiceImpl.java
+++ src/main/java/loverduck/clover/service/CrawlingServiceImpl.java
@@ -1,18 +1,15 @@
 package loverduck.clover.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import loverduck.clover.entity.Company;
 import loverduck.clover.entity.CrawlingData;
 import loverduck.clover.repository.CrawlingRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +18,7 @@ public class CrawlingServiceImpl implements CrawlingService {
     private final CrawlingRepository crawlingRepository;
     private final String prefix = "src/test/resources/json/";
 
-    private Map<String, Object> getFile(String path) {
+    private Map<String, ? > getFile(String path) {
         File file = new File(path);
         FileReader fileReader;
         StringBuilder sb;
@@ -37,7 +34,7 @@ public class CrawlingServiceImpl implements CrawlingService {
             }
 
             ObjectMapper objectMapper = new ObjectMapper();
-            return (Map<String, Object>) objectMapper.readValue(path, Map.class).get("compInfo");
+            return (Map<String, ?>) objectMapper.readValue(path, Map.class).get("compInfo");
         } catch (IOException e) {
 //            throw new RuntimeException(e);
             return null;
@@ -56,35 +53,39 @@ public class CrawlingServiceImpl implements CrawlingService {
     }
 
     @Override
-    public Map<String, Object> getFinancialData(String companyName) {
-        Map<String, Object> map = getFile(prefix + "financial/" + crawlingRepository.findByName(companyName));
-        return map;
+    public Map<String, List<Map<String, ?>>> getFinancialData(Company companyName) {
+        Map<String, ?> map = getFile(prefix + "financial/" + crawlingRepository.findByName(companyName.getName()));
+        if (map == null) return null;
+        Map<String,?> process = (Map<String, ?>) map.get("fnList");
+        Map<String, List<Map<String, ?>>> res = new HashMap<>();
+        res.put("consolidated", (List<Map<String, ?>>) process.get("1"));
+        res.put("separate", (List<Map<String, ?>>) process.get("2"));
+        return res;
     }
 
     @Override
-    public List<Map<String, Object>> getThreeYearFinanceList(String companyName) {
-        ArrayList<Map<String, Object>> list = new ArrayList<>();
-        Map<String, Object> obj = getFile(prefix + "info/" + crawlingRepository.findByName(companyName));
+    public List<Map<String, ?>> getThreeYearFinanceList(Company companyName) {
+        ArrayList<Map<String, ?>> list = new ArrayList<>();
+        Map<String, ?> obj = getFile(prefix + "info/" + crawlingRepository.findByName(companyName.getName()));
         if (obj == null) return null;
-        list.addAll((Collection<? extends Map<String, Object>>) obj.get("finance3Year"));
+        list.addAll((Collection<? extends Map<String, ?>>) obj.get("finance3Year"));
         return list;
     }
 
     @Override
-    public Map<String, Object> getCompanyDetail(String companyName) {
-        Map<String, Object> map = getFile(prefix + "/compInfo/" + crawlingRepository.findByName(companyName));
+    public Map<String, ?> getCompanyDetail(Company companyName) {
+        Map<String, ?> map = getFile(prefix + "/compInfo/" + crawlingRepository.findByName(companyName.getName()));
         if (map == null) return null;
-        return (Map<String, Object>) map.get("compInfo");
+        return (Map<String, ?>) map.get("compInfo");
     }
 
 
     @Override
-    public List<String> getKeyWordList(String companyName) {
+    public List<String> getKeyWordList(Company companyName) {
         ArrayList<String> list = new ArrayList<>();
-        Map<String, Object> map = getFile(prefix + "info/" + crawlingRepository.findByName(companyName));
+        Map<String, ?> map = getFile(prefix + "info/" + crawlingRepository.findByName(companyName.getName()));
         if (map == null) return list;
-        Map<String, Object> process = (Map<String, Object>) map.get("greenLight");
-        process.get("");
+        Map<String, ?> process = (Map<String, ?>) map.get("greenLight");
         list.addAll((Collection<? extends String>) process.get("GREEN"));
         return list;
     }
