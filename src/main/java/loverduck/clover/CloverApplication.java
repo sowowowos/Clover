8e8e13c1ea4d5a2c4ba01f6869f0c0e98a240f19
Gyeoul <clsmay.1st@gmail.com>
2023-05-20 22:35:15 +0900

    feat : 스케줄링 활성
    EnableScheduling 어노테이션으로 반복작업이 필요할 때 사용하는 용도

--- src/main/java/loverduck/clover/CloverApplication.java
+++ src/main/java/loverduck/clover/CloverApplication.java
@@ -2,7 +2,9 @@ package loverduck.clover;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class CloverApplication {
 
