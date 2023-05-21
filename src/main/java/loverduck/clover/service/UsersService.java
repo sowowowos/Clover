86150f07e6a80ba4c2276a04fb18d2621378315a
sowowowos <aceannie@naver.com>
2023-05-21 17:27:42 +0900

    feat: 카카오톡 로그인 구현 #51

--- src/main/java/loverduck/clover/service/UsersService.java
+++ src/main/java/loverduck/clover/service/UsersService.java
@@ -1,6 +1,6 @@
 package loverduck.clover.service;
 
-import loverduck.clover.entity.UserDetail;
+
 import loverduck.clover.entity.Users;
 
 public interface UsersService {
@@ -9,23 +9,16 @@ public interface UsersService {
 	/**
 	 * 회원가입하기
 	 */
-	int register(Users users,UserDetail userDetail);
+	int register(Users users);
 	
 	/**
 	 * 로그인채크
 	 */
 	Users logincheck(String email, String password);
 	
-	/**
-	 * 로그인
-	 */
-//	Users findByEmail(String email);
 
-	
 	/**
 	 * 진짜 - 이메일 중복 체크
-	 * @param email
-	 * @return
 	 */
 	boolean checkEmailExists(String email);
 	
