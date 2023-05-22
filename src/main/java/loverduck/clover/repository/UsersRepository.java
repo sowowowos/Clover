51d0713ddfa93ca35bceb706bbf0042d32014b28
sowowowos <aceannie@naver.com>
2023-05-22 10:49:41 +0900

    feat : 투자자/기업 개인정보 수정 페이지 생성 , 유저테이블 name 추가 #71
    Close #71

--- src/main/java/loverduck/clover/repository/UsersRepository.java
+++ src/main/java/loverduck/clover/repository/UsersRepository.java
@@ -2,8 +2,10 @@ package loverduck.clover.repository;
 
 import loverduck.clover.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UsersRepository extends JpaRepository<Users, Long>, QuerydslPredicateExecutor<Users> {
     @Query("select u from Users u where u.email = :email")
@@ -12,6 +14,10 @@ public interface UsersRepository extends JpaRepository<Users, Long>, QuerydslPre
     @Query("select u from Users u where u.userid = :userid")
     Users findByUserid(String userid);
 
-    boolean existsByEmail(String email);
+    @Transactional
+    @Modifying
+    @Query("update Users u set u.nickname = :nickname, u.password = :password, u.phone = :phone, u.postalCode = :postalCode, u.address = :address, u.detailAddress = :detailAddress where u.email = :email")
+    void updateUser(String nickname, String password, String phone, String postalCode, String address, String detailAddress, String email);
+    
 
 }
