9b27c65b9e60f0091d659bcb51878dea0a7aba7d
sowowowos <aceannie@naver.com>
2023-05-23 17:39:25 +0900

    feat : 기업 펀드 신청 페이지 화면 구현 #95

--- src/main/java/loverduck/clover/controller/FundingController.java
+++ src/main/java/loverduck/clover/controller/FundingController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -185,5 +186,13 @@ public class FundingController {
 	    return "/searchResults"; 
 	}
 	
+	/**
+	 * 펀딩 신청 - 기업 펀드신청 폼
+	 */
+	@GetMapping("/fundSubmitForm")
+	public String fundSubmitForm() {
+		
+		return "mypage/fundSubmitForm";
+	}
 	
 }
